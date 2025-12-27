644d8f0468764bee5d464f644151114e2ad36c96
Eduardo <eduardoviniciusjorge@gmail.com>
2025-12-26 23:15:07 -0300

    feat: add auth service with supabase

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, signal } from '@angular/core';
+import { SupabaseService } from './supabase.service';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  isAuthenticated = signal<boolean>(false);
+
+  constructor(private clientService: SupabaseService) {
+    const supabase = this.clientService.getClient();
+
+    // Initial session Check
+    supabase.auth.getSession().then((response) => {
+      const session = response.data.session;
+      this.isAuthenticated.set(session !== null);
+    });
+
+    // Observe changes in authentication state
+    supabase.auth.onAuthStateChange((_event, session) => {
+      this.isAuthenticated.set(session !== null);
+    });
+  }
+
+  async signInWithGoogle() {
+    const supabase = this.clientService.getClient();
+    const redirectTo = environment.supabase.redirectToHome;
+
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: redirectTo,
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        }
+      }
+    });
+  }
+
+  async signOut() {
+    const supabase = this.clientService.getClient();
+    await supabase.auth.signOut();
+  }
+}
