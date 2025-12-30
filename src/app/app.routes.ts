d0b2cc434358a2fa1453208e94af2b8eede4aa79
Eduardo <eduardoviniciusjorge@gmail.com>
2025-12-30 18:58:49 -0300

    feat: add routes and auth guard for routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CotacoesPageComponent } from './pages/cotacoes-page/cotacoes-page.component';
+import { AdicionarItemPageComponent } from './pages/adicionar-item-page/adicionar-item-page.component';
+import { FornecedoresPageComponent } from './pages/fornecedores-page/fornecedores-page.component';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'cotacoes',
+    component: CotacoesPageComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'adicionar-item',
+    component: AdicionarItemPageComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'fornecedores',
+    component: FornecedoresPageComponent,
+    canActivate: [authGuard]
+  }
+];
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { Router, CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+
+export const authGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if(!authService.isAuthenticated()){
+    return router.createUrlTree(['/login']);
+  }
+  return true;
+};
+
+
