2694f83040d63fd4bb8e7372c15c7f581bd0a4f8
Eduardo <eduardoviniciusjorge@gmail.com>
2025-12-30 18:57:05 -0300

    feat: adding method signal isAuthenticated

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,10 +10,15 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  private authService = inject(AuthService);
+  
   isMenuOpen = false;
   showNavbar = true;
-  
-
+  isAuthenticated = this.authService.isAuthenticated;
   toggleMenu(): void { this.isMenuOpen = !this.isMenuOpen; }
   closeMenu(): void { this.isMenuOpen = false; }
+
+  async logout(): Promise<void> {
+    await this.authService.signOut();
+  }
 }
